c0250f8a783bfad38deafd32b61670a7a5e36a86
Kyle Taylor <kyletaylored@gmail.com>
2020-09-18 01:38:55 -0500

    update deploy and workflow

--- scripts/shell/deploy-sequence.sh
+++ scripts/shell/deploy-sequence.sh
@@ -7,9 +7,15 @@
 set -e
 
 echo -e "Starting ${1}";
+
+# Check site upstream for updates, apply
 terminus site:upstream:clear-cache $1
 terminus upstream:update:status "${1}.dev"
 terminus upstream:updates:apply "${1}.dev"
+
+# Run drush updates on dev, clear cache
 terminus drush "${1}.dev" -- updb -y
 terminus env:clear-cache "${1}.dev"
+
+# Deploy code to test
 terminus env:deploy ${1}.test --cc --updatedb --note ''
